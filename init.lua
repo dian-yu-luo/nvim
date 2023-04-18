86a1603efd3921549309602056808778c517a6d2
dianyuluo <1739556970@qq.com>
2023-04-18 11:34:38 +0000

    fix

--- init.lua
+++ init.lua
@@ -3,6 +3,7 @@ require('plugins')
 vim.wo.relativenumber = true
 vim.wo.number = true
 vim.opt.cursorline = true
+vim.g.mapleader = ' '
 
 vim.api.nvim_set_keymap('i', '<C-a>', '<Home>', { noremap = true })
 vim.api.nvim_set_keymap('i', '<C-e>', '<End>', { noremap = true })
@@ -19,7 +20,8 @@ vim.api.nvim_set_keymap('c', '<Esc>b', '<S-Left>', { noremap = true })
 vim.api.nvim_set_keymap('c', '<Esc>f', '<S-Right>', { noremap = true })
 vim.api.nvim_set_keymap('c', "<C-y>", '\"+p', { noremap = true, silent = true })
 function delete_to_eol()
-    if vim.fn.col('.') == #(vim.fn.getline('.')) then
+    local line = vim.fn.getline('.')
+    if #line == 0 or vim.fn.col('.') == #line + 1 then
         vim.api.nvim_del_current_line()
     else
         vim.api.nvim_feedkeys('d$', 'n', true)
@@ -27,13 +29,24 @@ function delete_to_eol()
 end
 
 vim.api.nvim_set_keymap('n', '<C-k>', ':lua delete_to_eol()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('i', '<C-k>', ':lua delete_to_eol()<CR>', { noremap = true, silent = true })
 
 vim.o.guifont = '霞鹜文楷等宽:h16'
 vim.o.clipboard = 'unnamed'
 
 require('dashboard').setup()
+vim.api.nvim_set_keymap('n', '<leader>d', ':Dashboard<CR>', { noremap = true, silent = true })
 
-require('wilder').setup({ modes = { ':', '/', '?' } })
+-- local wilder = require('wilder')
+-- wilder.setup({modes = {':', '/', '?'}})
+
+local telescope = require('telescope')
+vim.api.nvim_set_keymap('n', '<leader>ff', "<cmd>lua require('telescope.builtin').find_files()<CR>",
+    { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>fc', "<cmd>lua require('telescope.builtin').commands()<CR>",
+    { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>fh', "<cmd>lua require('telescope.builtin').command_history()<CR>",
+    { noremap = true, silent = true })
 
 vim.cmd("autocmd InsertLeave * silent !zenhan 0")
 os.execute('zenhan.exe 0')
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -4,7 +4,11 @@ return require('packer').startup(function(use)
   use 'wbthomason/packer.nvim'
   use 'nvim-tree/nvim-web-devicons'
   use 'glepnir/dashboard-nvim'
-  use 'nvim-telescope/telescope.nvim'
+  use {
+    'nvim-telescope/telescope.nvim', tag = '0.1.1',
+    requires = { { 'nvim-lua/plenary.nvim' } }
+  }
+
   use 'gelguy/wilder.nvim'
   use 'neovim/nvim-lspconfig'
 end)
