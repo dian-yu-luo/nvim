022fd111a0095b2d363cc0b105c43e0b5fe311bf
dianyuluo <1739556970@qq.com>
2023-04-18 18:14:55 +0000

    更新插件whichkey org

--- init.lua
+++ init.lua
@@ -9,6 +9,8 @@ vim.api.nvim_set_keymap('i', '<C-a>', '<Home>', { noremap = true })
 vim.api.nvim_set_keymap('i', '<C-e>', '<End>', { noremap = true })
 vim.api.nvim_set_keymap('i', '<C-f>', '<Right>', { noremap = true })
 vim.api.nvim_set_keymap('i', '<C-b>', '<Left>', { noremap = true })
+vim.api.nvim_set_keymap('i', '<C-n>', '<Down>', { noremap = true })
+vim.api.nvim_set_keymap('i', '<C-p>', '<Up>', { noremap = true })
 vim.api.nvim_set_keymap('i', '<C-k>', '<C-o>ddk', { noremap = true })
 vim.api.nvim_set_keymap('i', '<C-d>', '<Delete>', { noremap = true })
 vim.api.nvim_set_keymap('i', '<C-/>', '<C-u>', { noremap = true })
@@ -29,7 +31,7 @@ function delete_to_eol()
 end
 
 vim.api.nvim_set_keymap('n', '<C-k>', ':lua delete_to_eol()<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('i', '<C-k>', ':lua delete_to_eol()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('i', '<C-k>', '<Esc>:lua delete_to_eol()<CR>', { noremap = true, silent = true })
 
 vim.o.guifont = '霞鹜文楷等宽:h16'
 vim.o.clipboard = 'unnamed'
@@ -47,6 +49,24 @@ vim.api.nvim_set_keymap('n', '<leader>fc', "<cmd>lua require('telescope.builtin'
     { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>fh', "<cmd>lua require('telescope.builtin').command_history()<CR>",
     { noremap = true, silent = true })
+
+require("which-key").setup {}
+
+require('orgmode').setup_ts_grammar()
+require 'nvim-treesitter.configs'.setup {
+    -- If TS highlights are not enabled at all, or disabled via ``disable`` prop, highlighting will fallback to default Vim syntax highlighting
+    highlight = {
+        enable = true,
+        disable = { 'org' },                           -- Remove this to use TS highlighter for some of the highlights (Experimental)
+        additional_vim_regex_highlighting = { 'org' }, -- Required since TS highlighter doesn't support all syntax features (conceal)
+    },
+    ensure_installed = { 'org' },                      -- Or run :TSUpdate org
+}
+
+require('orgmode').setup({
+    org_agenda_files = { '~/Dropbox/org/*', '~/my-orgs/**/*' },
+    org_default_notes_file = '~/Dropbox/org/refile.org',
+})
 
 vim.cmd("autocmd InsertLeave * silent !zenhan 0")
 os.execute('zenhan.exe 0')
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -4,11 +4,11 @@ return require('packer').startup(function(use)
   use 'wbthomason/packer.nvim'
   use 'nvim-tree/nvim-web-devicons'
   use 'glepnir/dashboard-nvim'
-  use {
-    'nvim-telescope/telescope.nvim', tag = '0.1.1',
-    requires = { { 'nvim-lua/plenary.nvim' } }
-  }
-
+  use 'nvim-lua/plenary.nvim'
+  use 'nvim-telescope/telescope.nvim'
+  use "folke/which-key.nvim"
+  use 'nvim-treesitter/nvim-treesitter'
+  use 'nvim-orgmode/orgmode'
   use 'gelguy/wilder.nvim'
   use 'neovim/nvim-lspconfig'
 end)
