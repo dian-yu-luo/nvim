12a287fd125c7f76e3bb209a081fcbc677e7c780
dianyuluo <1739556970@qq.com>
2023-05-09 06:24:55 +0000

    自动管理插件

--- init.lua
+++ init.lua
@@ -1,9 +1,3 @@
-if vim.fn.empty(vim.fn.glob('~/.local/share/nvim/site/pack/packer/start/packer.nvim')) > 0 then
-    vim.cmd(
-        '!git clone https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim')
-    vim.cmd('autocmd VimEnter * PackerInstall')
-end
--- 自定义配置的模块
 require('plugins')
 require('core')
 require('config')
