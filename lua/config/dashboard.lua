aa61849d3776c2b45058e14a8997a9d38eca66b7
dianyuluo <1739556970@qq.com>
2023-05-11 09:43:33 +0000

    常用操作放到dashboard里面

--- lua/config/dashboard.lua
+++ lua/config/dashboard.lua
@@ -1,2 +1,32 @@
-require('dashboard').setup()
+require('dashboard').setup({
+    theme = 'hyper',
+    config = {
+        week_header = {
+            enable = true,
+        },
+        shortcut = {
+            { desc = '󰊳 Update', group = '@property', action = 'PackerSync', key = 'uu' },
+            {
+                icon = ' ',
+                icon_hl = '@variable',
+                desc = 'Files',
+                group = 'Label',
+                action = 'Telescope find_files',
+                key = 'f',
+            },
+            {
+                desc = ' Apps',
+                group = 'DiagnosticHint',
+                action = 'Telescope app',
+                key = 'a',
+            },
+            {
+                desc = ' dotfiles',
+                group = 'Number',
+                action = 'Telescope dotfiles',
+                key = 'd',
+            },
+        },
+    },
+})
 vim.api.nvim_set_keymap('n', '<leader>d', ':Dashboard<CR>', { noremap = true, silent = true })
