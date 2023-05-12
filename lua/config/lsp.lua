6ecea516486957a522f09bde73a6c3c68287920c
dianyuluo <1739556970@qq.com>
2023-05-12 04:58:35 +0000

    尝试lua 配置

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -1,1 +1,20 @@
 require 'lspconfig'.pyright.setup {}
+require 'lspconfig'.lua_ls.setup {
+    settings = {
+        Lua = {
+            runtime = {
+                version = 'LuaJIT',
+            },
+            diagnostics = {
+                globals = { 'vim' },
+            },
+            workspace = {
+                -- library = vim.api.nvim_get_runtime_file("", true),
+                library= "~/.config/nvim"
+            },
+            telemetry = {
+                enable = false,
+            },
+        },
+    },
+}
