6ae4ff63a15b0944a0d05e0bab76fbf68181104e
dianyuluo <1739556970@qq.com>
2023-05-14 21:37:07 +0000

    -- Remember cursor position

--- init.lua
+++ init.lua
@@ -2,6 +2,12 @@ require('plugins')
 require('core')
 require('config')
 
+vim.cmd([[
+    autocmd BufReadPost *
+        if line("'\"") > 0 and line ("'\"") <= line("$") then
+            exe "normal! g'\""
+        end
+]])
 
 vim.cmd("autocmd InsertLeave * silent !zenhan 0")
 os.execute('zenhan.exe 0')
