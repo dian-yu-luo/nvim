1075bf1bf811b43e7d38f3307c8819627c4e4248
dianyuluo <1739556970@qq.com>
2023-05-19 13:18:11 +0000

    win specific

--- init.lua
+++ init.lua
@@ -3,5 +3,8 @@ require('core')
 require('config')
 
 
-vim.cmd("autocmd InsertLeave * silent !zenhan 0")
-os.execute('zenhan.exe 0')
+if vim.fn.has("win32") == 1 then
+    -- Convert input to full-width characters after leaving insert mode
+    vim.cmd("autocmd InsertLeave * silent !zenhan 0")
+    os.execute('zenhan.exe 0')
+ end
